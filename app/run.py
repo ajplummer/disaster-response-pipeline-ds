901089805b998207df8d0497a69e75bf231e0da3
ajplummer <andrew.j.plummer@gmail.com>
2021-04-12 17:45:00 -0400

    Add files via upload
    Updated the code to include a visualization on the go.html page.

--- app/run.py
+++ app/run.py
@@ -1,6 +1,8 @@
 import json
 import plotly
 import pandas as pd
+import joblib
+import pickle
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -8,7 +10,6 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
 
@@ -30,7 +31,9 @@ engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('Messages', engine)
 
 # load model
-model = joblib.load("../models/classifier.pkl")
+with open("../models/classifier.pkl", 'rb') as file:
+    model = pickle.load(file)
+#model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -42,7 +45,6 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -83,12 +85,41 @@ def go():
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
+    print(classification_labels)
+
+    # create visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=df.columns[4:],
+                    y=classification_labels
+                )
+            ],
+
+            'layout': {
+                'title': 'Genre Classifications',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        }
+    ]
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
 
     # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
-        classification_result=classification_results
+        classification_result=classification_results,
+        ids=ids,
+        graphJSON=graphJSON
     )
 
 
